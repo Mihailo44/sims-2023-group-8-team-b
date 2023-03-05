22b79e0fc26e3b6881273045d6033d7e31e18a33
milica <milicakljajic1@gmail.com>
2023-03-05 14:51:01 +0100

    Implementirane klase Tourist.cs i Address.cs

---
+++ TouristAgency/TouristAgency/Model/Address.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TouristAgency.Model
+{
+    internal class Address
+    {
+        private string _street;
+        private string _streetNumber;
+        private string _city;
+        private string _country;
+
+        public Address()
+        {
+
+        }
+
+        public string Street
+        { 
+            get { return _street; } 
+            set {  _street = value; } 
+        }
+
+        public string StreetNumber
+        { 
+            get { return _streetNumber; } 
+            set { _streetNumber = value; } 
+        }
+
+        public string City 
+        { 
+            get { return _city; } 
+            set { _city = value; } 
+        }
+
+        public string Country 
+        { 
+            get { return _country; } 
+            set { _country = value; } 
+        }
+    }
+}
---
+++ TouristAgency/TouristAgency/Model/Tourist.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TouristAgency.Model
+{
+    internal class Tourist
+    {
+        private int _touristID;
+        private string _username;
+        private string _password;
+        private string _firstName;
+        private string _lastName;
+        private string _email;
+        private Address _address;
+        private string _phone;
+        
+        public Tourist()
+        { 
+        
+        }
+
+        public int TouristID
+        { 
+            get { return _touristID; }
+            set { _touristID = value; }
+        }
+
+        public string Username
+        {
+            get { return _username; }
+            set { _username = value; }
+        }
+
+        public string Password
+        {
+            get { return _password; }
+            set { _password = value; }
+        }
+
+        public string FirstName
+        { 
+            get { return _firstName; } 
+            set {  _firstName = value; } 
+        }
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+
+        public string Email
+        { 
+            get { return _email; }
+            set { _email = value; } 
+        }
+
+        public Address Address
+        {
+            get { return _address; }
+            set { _address = value; }
+        }
+
+        public string Phone
+        {
+            get { return _phone; }
+            set { _phone = value; }
+        }
+    }
+}
