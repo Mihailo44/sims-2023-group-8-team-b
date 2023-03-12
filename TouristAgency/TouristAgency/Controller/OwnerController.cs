2b5f68c271ae93cd605540d94707d878bc54a7d3
Mihailo <zupermancile@gmail.com>
2023-03-12 12:04:08 +0100

    dodate klase AccommodationController,OwnerController,ReservationController

---
+++ TouristAgency/TouristAgency/Controller/OwnerController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TouristAgency.Model;
+using TouristAgency.Model.DAO;
+using TouristAgency.Interfaces;
+
+namespace TouristAgency.Controller
+{
+    public class OwnerController
+    {
+        private readonly OwnerDAO _owner;
+
+        public OwnerController()
+        {
+            _owner = new OwnerDAO();
+        }
+
+        public List<Owner> GetAll()
+        {
+            return _owner.GetAll();
+        }
+
+        public void Create(Owner newOwner)
+        {
+            _owner.Create(newOwner);
+        }
+
+        public void Update(Owner newOwner,int id)
+        {
+            _owner.Update(newOwner, id);
+        }
+
+        public void Delete(Owner newOwner)
+        {
+            _owner.Delete(newOwner.ID);
+        }
+
+        public void Subscribe(IObserver observer)
+        {
+            _owner.Subscribe(observer);
+        }
+    }
+}
