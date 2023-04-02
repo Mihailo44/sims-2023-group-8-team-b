31dad0f00c7e71735696dfdf04668b4c5a678c06
ognjenm01 <ognjenmilojevic2001@gmail.com>
2023-04-02 16:31:17 +0200

    Created class voucher and deleted test

--- TouristAgency/TouristAgency/View/Display/TourDisplay.xaml.cs
+++ TouristAgency/TouristAgency/View/Display/TourDisplay.xaml.cs
@@ -17,7 +17,6 @@ using System.Windows.Shapes;
 using TouristAgency.ViewModel;
 using TouristAgency.Model;
 using TouristAgency.Storage;
-using TouristAgency.Test;
 
 namespace TouristAgency.View.Display
 {
