6e82329f9d901862a651535d46d56e12ed0bfc45
milica <milicakljajic1@gmail.com>
2023-05-01 02:01:09 +0200

    Created form for tour request

---
+++ TouristAgency/TouristAgency/TourRequests/TourRequestViewModel.cs
@@ -0,0 +1,14 @@
+﻿using TouristAgency.Users;
+
+namespace TouristAgency.TourRequests
+{
+    internal class TourRequestViewModel
+    {
+        private Tourist _loggedInTourist;
+
+        public TourRequestViewModel(Tourist tourist)
+        {
+            _loggedInTourist = tourist;
+        }
+    }
+}
