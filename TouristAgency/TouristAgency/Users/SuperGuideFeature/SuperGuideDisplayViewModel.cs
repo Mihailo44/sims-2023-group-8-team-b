7cc2408ee7c28b9a8a7d9dcc0f8ef45c751fba0a
Ognjen <ognjenmilojevic2001@gmail.com>
2023-06-03 19:36:31 +0200

    interface changes

---
+++ TouristAgency/TouristAgency/Users/SuperGuideFeature/SuperGuideDisplayViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using TouristAgency.Base;
+using TouristAgency.Tours;
+using TouristAgency.Users.HomeDisplayFeature;
+using TouristAgency.Util;
+using TouristAgency.Vouchers;
+
+namespace TouristAgency.Users.SuperGuideFeature
+{
+    public class SuperGuideDisplayViewModel : BurgerMenuViewModelBase
+    {
+        private App _app;
+        private Guide _guide;
+        public DelegateCommand CloseCmd { get; set; }
+        public SuperGuideDisplayViewModel()
+        {
+            _app = (App)Application.Current;
+            _guide = _app.LoggedUser;
+            MenuVisibility = "Hidden";
+            InstantiateCommands();
+            InstantiateMenuCommands();
+        }
+
+        private void InstantiateCommands()
+        {
+            CloseCmd = new DelegateCommand(param => CloseExecute(), param => CanCloseExecute());
+        }
+
+        public bool CanCloseExecute()
+        {
+            return true;
+        }
+        public void CloseExecute()
+        {
+            _app.CurrentVM = new GuideHomeViewModel();
+        }
+    }
+}
