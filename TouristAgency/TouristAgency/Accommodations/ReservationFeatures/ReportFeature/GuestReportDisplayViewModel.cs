c718db2286464f57033902179cd4fcaf25dba051
Ljubisa-Peric <peric.ljubo01@gmail.com>
2023-06-06 19:28:05 +0200

    Finished report feature and wizard for hci

--- TouristAgency/TouristAgency/Accommodations/ReservationFeatures/ReportFeature/GuestReportDisplayViewModel.cs
+++ TouristAgency/TouristAgency/Accommodations/ReservationFeatures/ReportFeature/GuestReportDisplayViewModel.cs
@@ -34,6 +34,7 @@ namespace TouristAgency.Accommodations.ReservationFeatures.ReportFeature
         private AccommodationService _accommodationService;
 
         public DelegateCommand GenerateRegularReportCmd { get; set; }
+        public DelegateCommand GenerateCanceledReportCmd { get; set; }
         public DelegateCommand AccommodationDisplayCmd { get; set; }
         public DelegateCommand PostponementRequestDisplayCmd { get; set; }
         public DelegateCommand OwnerReviewCreationCmd { get; set; }
@@ -79,6 +80,7 @@ namespace TouristAgency.Accommodations.ReservationFeatures.ReportFeature
             ForumDisplayCmd = new DelegateCommand(param => OpenForumDisplayCmdExecute(), param => CanOpenForumDisplayCmdExecute());
             GuestReportDisplayCmd = new DelegateCommand(param => OpenGuestReportDisplayCmdExecute(), param => CanOpenGuestReportDisplayCmdExecute());
             GenerateRegularReportCmd = new DelegateCommand(param => GenerateRegularReportCmdExecute(), param => CanGenerateRegularReportCmdExecute());
+            GenerateCanceledReportCmd = new DelegateCommand(param => GenerateCanceledReportCmdExecute(), param => CanGenerateCanceledReportCmdExecute());
         }
 
         public DateTime Start
@@ -242,6 +244,43 @@ namespace TouristAgency.Accommodations.ReservationFeatures.ReportFeature
             PdfDocument newPdf = renderer.RenderHtmlAsPdf(HtmlString);
             newPdf.SaveAs(fileName);
             MessageBox.Show("Successfully created regular reservation report.");
+        }
+
+        public bool CanGenerateCanceledReportCmdExecute()
+        {
+            return true;
+        }
+
+        public void GenerateCanceledReportCmdExecute()
+        {
+            string fileName = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName + "/Resources/PDF/CanceledReservationReport" + DateTime.Now.ToString("ddMMMyyyyHHmmss") + ".pdf";
+            IronPdf.License.LicenseKey = "IRONPDF.OGNJENMILOJEVIC2001.11160-6DF7F18EC0-PQ5YOKIDDHF4X-PDSDZ265OZUC-DSOYMURM3EY4-U3X2FINUULAL-T4IS3FXVY3HX-OVXCDF-TUNPAYY3BA2KEA-DEPLOYMENT.TRIAL-FU7CHU.TRIAL.EXPIRES.05.JUL.2023";
+            string HtmlString = "<h1>Canceled reservations report</h1>" +
+                "<br>" +
+                "<p>Generated at: " + DateTime.Now + "</p>" +
+                "<p>Requested by: " + _loggedInGuest.FirstName + " " + _loggedInGuest.LastName + "</p>" +
+                "<p>Date ranges: " + Start.Date.ToString() + " - " + End.Date.ToString() + "</p>" +
+                "<br><hr>" +
+                "<table style='width:100%;'>" +
+                "<tr style='border:1px solid black'><th>Accommodation name</th> <th>Location</th> <th>Owner</th> <th>Start date</th>" +
+                "<th>End date</th></tr>";
+            int count = 0;
+            foreach (Reservation reservation in _reservationService.GetInCanceledInterval(Start, End, _loggedInGuest.ID))
+            {
+                count++;
+                HtmlString += "<tr style='text-align:center;border:1px solid black'><td>" + reservation.Accommodation.Name + "</td>" +
+                            "<td>" + reservation.Accommodation.Location.Country + ", " + reservation.Accommodation.Location.City + "</td>" +
+                            "<td>" + reservation.Accommodation.Owner.FirstName + ", " + reservation.Accommodation.Owner.LastName + "</td>" + "<td>" + reservation.Start + "</td>" +
+                            "<td>" + reservation.End + "</td></tr>";
+            }
+
+            HtmlString += "</table><br><hr><br><p>Total reservations: " + count + "</p>";
+
+            ChromePdfRenderer renderer = new ChromePdfRenderer();
+
+            PdfDocument newPdf = renderer.RenderHtmlAsPdf(HtmlString);
+            newPdf.SaveAs(fileName);
+            MessageBox.Show("Successfully created canceled reservation report.");
         }
     }
 }
---
+++ TouristAgency/TouristAgency/Converter/GuestUsefulCommentConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace TouristAgency.Converter
+{
+    public class GuestUsefulCommentConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool useful = (bool)value;
+            if (useful == true)
+            {
+                return "../../../Resources/Image/star-guest.png";
+            }
+            else
+            {
+                return "../../../Resources/Image/blank.png";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
